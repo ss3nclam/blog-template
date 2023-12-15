f72e036f1f7890199c5a89db5262595c1b08b27a
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2023-12-16 02:40:16 +0400

    modified

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/example')
+def example():
+    return render_template('example.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
