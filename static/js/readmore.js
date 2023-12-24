8ca5fef0e9d98363658f3f2a8d7ed06bbe12fecc
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2023-12-24 06:27:20 +0400

    updated

--- static/js/readmore.js
+++ static/js/readmore.js
@@ -1,11 +1,21 @@
-let container = document.getElementById("container");
-let button = document.getElementById("button");
+const array = Array.from(document.getElementsByClassName('post_block'));
 
-button.addEventListener("click", function() {
-    if (container.scrollHeight > container.style.maxHeight) {
-        button.style.opacity = 0;
-        container.style.height = container.scrollHeight + "px";
-        // button.remove();
+array.forEach(function(post) {
+    let post_id = post.attributes.post_id.value;
+    let container = document.getElementById("post_" + post_id + "_content");
+    let button = document.getElementById("post_" + post_id + "_readmore_btn");
+    let btn_block = document.getElementById("post_" + post_id + "_readmore_block");
+
+    let scroll_height = container.scrollHeight
+    let max_height = +window.getComputedStyle(container).maxHeight.replace('px', '')
+
+    if (max_height < scroll_height) {
+        button.addEventListener("click", function() {
+            container.style.maxHeight = scroll_height + "px";
+            button.style.opacity = 0;
+        });
     } else {
-        console.log('jopa')
-}});
+        button.style.display = 'none'
+        btn_block.style.justifyContent = 'right'
+    }
+});
