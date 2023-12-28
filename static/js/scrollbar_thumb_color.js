eaad01f3fe6ae4094b5abd80d874a25dc8d6b3ca
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2023-12-28 17:22:36 +0400

    updated

--- static/js/scrollbar_thumb_color.js
+++ static/js/scrollbar_thumb_color.js
@@ -1,7 +1,7 @@
 document.onscroll = function() {
-    document.documentElement.style.setProperty('--scroll-bar-color', 'var(--accent-color)');
+    document.body.style.setProperty('--scroll-bar-color', 'var(--accent-color)');
 };
 
 document.onscrollend = function() {
-    document.documentElement.style.setProperty('--scroll-bar-color', 'var(--border-color)');
+    document.body.style.setProperty('--scroll-bar-color', 'var(--border-color)');
 };
