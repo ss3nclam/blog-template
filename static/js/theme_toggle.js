3437ba67710eb4155f4429a077325a6edb81ad04
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-01-03 20:59:22 +0400

    updated

--- static/js/theme_toggle.js
+++ static/js/theme_toggle.js
@@ -1,6 +1,4 @@
-let icon = color_scheme_switcher_icon.classList.value
-
 color_scheme_switcher.onclick = function () {
-    icon = document.body.classList.contains('dark-theme') ? 'bi-sun-fill' : 'bi-moon-stars-fill';
     document.body.classList.toggle('dark-theme');
+    color_scheme_switcher_icon.classList.toggle('bi-sun-fill')
 };
